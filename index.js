d116c7f13bd151c30a2ff55c7bafc8cfcab32c9b
Brandon Sears <brandonsears8@gmail.com>
2018-03-01 16:12:39 -0500

    finished create and show joke

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 // require all our dependencies
 const express = require('express')
 const hbs = require('hbs')
+const parser = require('body-parser')
 const Joke = require('./models/Joke')
+const jokesController = require('./controllers/joke')
 // create and setup express app
 const app = express()
 
 // configure express to use hbs
 app.set('view engine', 'hbs')
+
+// configure express to use body parser
+app.use(parser.urlencoded({ extended: true }))
+// app.use(parser.json())
 
 // define a route
 app.get('/', (req, res) => {
@@ -15,5 +21,6 @@ app.get('/', (req, res) => {
   })
 })
 
+app.use('/jokes', jokesController)
 // start the server
 app.listen(3000, () => console.log('running'))
